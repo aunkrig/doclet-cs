be830d1912af1faba7cc3b73b427cce7a5403bc8
Arno Unkrig <aunkrig@users.noreply.github.com>
2014-12-28 11:13:22 +0000

    Competed migration to the CS DOCLET.

--- src/de/unkrig/doclet/cs/CsDoclet.java
+++ src/de/unkrig/doclet/cs/CsDoclet.java
@@ -32,27 +32,51 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.DocErrorReporter;
+import com.sun.javadoc.PackageDoc;
 import com.sun.javadoc.RootDoc;
 
-public class CsDoclet {
+import de.unkrig.doclet.cs.Mediawiki.Longjump;
 
-    interface ConsumerWhichThrows<T, EX extends Exception> { void consume(T durable) throws EX; }
+/**
+ * A doclet that creates ECLIPSE-CS metadata files and/or documentation for CheckStyle rules in MediaWiki markup
+ * format.
+ */
+public final
+class CsDoclet {
 
+    private CsDoclet() {}
+
+    /** An object that somehow 'consumes' another object, or objects. */
+    interface ConsumerWhichThrows<T, EX extends Throwable> { void consume(T durable) throws EX; }
+
+    /**
+     * See <a href="https://docs.oracle.com/javase/6/docs/technotes/guides/javadoc/doclet/overview.html">"Doclet
+     * Overview"</a>.
+     */
     public static int
     optionLength(String option) {
 
-        if (option.equals("-checkstyle-metadata-dir")) return 2;
-        if (option.equals("-mediawiki-dir")) return 2;
+        if ("-checkstyle-metadata-dir".equals(option)) return 2;
+        if ("-mediawiki-dir".equals(option))           return 2;
 
         return 0;
     }
 
+    /**
+     * See <a href="https://docs.oracle.com/javase/6/docs/technotes/guides/javadoc/doclet/overview.html">"Doclet
+     * Overview"</a>.
+     */
     public static boolean
     start(final RootDoc rootDoc) throws IOException {
 
         File checkstyleMetadataDir = null;
-        File mediawikiDir = null;
+        File mediawikiDir          = null;
 
         for (String[] option : rootDoc.options()) {
             if ("-checkstyle-metadata-dir".equals(option[0])) {
@@ -75,30 +99,62 @@ public class CsDoclet {
             );
         }
 
-        if (checkstyleMetadataDir != null) {
-            CsDoclet.generateCheckstyleMetadata(rootDoc, checkstyleMetadataDir);
-        }
+        // Process all specified packages.
+        for (PackageDoc pd : rootDoc.specifiedPackages()) {
+            String checkstylePackage = pd.name();
 
-        if (mediawikiDir != null) {
-            Mediawiki.generate(rootDoc, mediawikiDir);
+            // Collect all classes in that package.
+            SortedMap<String, ClassDoc> classDocs = new TreeMap<String, ClassDoc>();
+            for (ClassDoc classDoc : rootDoc.classes()) {
+                String packageName = classDoc.containingPackage().name();
+
+                if (packageName.equals(checkstylePackage)) {
+                    classDocs.put(classDoc.name(), classDoc);
+                }
+            }
+
+            // Generate 'checkstyle-metadata.xml' and 'checkstyle-metadata.properties' for the package.
+            if (checkstyleMetadataDir != null) {
+                CsDoclet.generateCheckstyleMetadata(
+                    classDocs.values(),
+                    new File(checkstyleMetadataDir, checkstylePackage.replace('.', File.separatorChar)),
+                    rootDoc
+                );
+            }
+
+            // Generate MediaWiki markup documents for each class in the package.
+            if (mediawikiDir != null) {
+                for (ClassDoc classDoc : classDocs.values()) {
+                    try {
+                        Mediawiki.generate(classDoc, mediawikiDir, rootDoc);
+                    } catch (Longjump e) {
+                        ;
+                    }
+                }
+            }
         }
 
         return true;
     }
 
     private static void
-    generateCheckstyleMetadata(final RootDoc rootDoc, final File todir) throws IOException {
+    generateCheckstyleMetadata(
+        final Collection<ClassDoc> classDocs,
+        final File                 todir,
+        final DocErrorReporter     errorReporter
+    ) throws IOException {
 
         CsDoclet.printToFile(
             new File(todir, "checkstyle-metadata.xml"),
             Charset.forName("UTF-8"),
             new ConsumerWhichThrows<PrintWriter, IOException>() {
 
-            @Override public void
-            consume(final PrintWriter cmx) {
-                CheckstyleMetadata.checkstyleMetadataXml(rootDoc, cmx);
+                @Override public void
+                consume(final PrintWriter cmx) {
+                    CheckstyleMetadata.checkstyleMetadataXml(classDocs, cmx, errorReporter);
+                }
             }
-        });
+        );
 
         CsDoclet.printToFile(
             new File(todir, "checkstyle-metadata.properties"),
@@ -107,24 +163,44 @@ public class CsDoclet {
 
                 @Override public void
                 consume(PrintWriter cmp) throws IOException {
-                    CheckstyleMetadata.checkstyleMetadataProperties(rootDoc, cmp);
+                    CheckstyleMetadata.checkstyleMetadataProperties(classDocs, cmp, errorReporter);
+                }
+            }
+        );
+
+        CsDoclet.printToFile(
+            new File(todir, "messages.properties"),
+            Charset.forName("ISO-8859-1"),
+            new ConsumerWhichThrows<PrintWriter, IOException>() {
+
+                @Override public void
+                consume(PrintWriter mp) {
+                    CheckstyleMetadata.messagesProperties(classDocs, mp, errorReporter);
                 }
             }
         );
     }
 
-    public static <EX extends Exception> void
+    /**
+     * Creates the named {@code file}, lets the {@code printer} print text to it, and closes the file.
+     * <p>
+     *
+     *
+     * @param charset The charset to be used for printing
+     * @throws EX     The throwable that the {@code printer} may throw
+     */
+    public static <EX extends Throwable> void
     printToFile(
         File                                 file,
         Charset                              charset,
-        ConsumerWhichThrows<PrintWriter, EX> consumer
+        ConsumerWhichThrows<PrintWriter, EX> printer
     ) throws IOException, EX {
 
         File newFile = new File(file.getParentFile(), "." + file.getName() + ".new");
 
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(newFile), charset));
         try {
-            consumer.consume(pw);
+            printer.consume(pw);
             pw.close();
             if (file.exists() && !file.delete()) {
                 throw new IOException("Could not delete existing file '" + file + "'");
@@ -133,15 +209,20 @@ public class CsDoclet {
                 throw new IOException("Could not rename '" + newFile + "' to '" + file + "'");
             }
         } catch (RuntimeException re) {
-            try { pw.close(); } catch (Exception e) {}
-            newFile.delete();
-
-            throw re;
-        } catch (Exception e) {
             try { pw.close(); } catch (Exception e2) {}
             newFile.delete();
 
-            @SuppressWarnings("unchecked") EX tmp = (EX) e;
+            throw re;
+        } catch (Error e) { // SUPPRESS CHECKSTYLE IllegalCatch
+            try { pw.close(); } catch (Exception e2) {}
+            newFile.delete();
+
+            throw e;
+        } catch (Throwable t) { // SUPPRESS CHECKSTYLE IllegalCatch
+            try { pw.close(); } catch (Exception e2) {}
+            newFile.delete();
+
+            @SuppressWarnings("unchecked") EX tmp = (EX) t;
             throw tmp;
         }
     }
